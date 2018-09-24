25c07e63842d4b467f38abac43296ef4e90982ca
alanjeanpierre <ajeanpierre825@gmail.com>
2018-09-23 17:58:38 -0700

    Add file size info to tests

--- tests.sh
+++ tests.sh
@@ -10,7 +10,9 @@ do
     then
         echo -e "\e[31m$file: Test fail :c\e[39m"
     else
-        echo -e "\e[92m$file: Test past c:\e[39m"
+        size1="$(stat -c %s $file)"
+        size2="$(stat -c %s tmp)"
+        echo -e "\e[92m$file: Test passed c: $size1 -> $size2\e[39m"
     fi
     rm "out" "tmp"
 done
